b386fc3755b459a763bd3f32dbc691139eb9691c
Michal Vácha <michal.vacha@live.com>
2018-05-10 11:59:00 +0200

    * updated version to 1.3.7

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -17,6 +17,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.2.0")]
-[assembly: AssemblyFileVersion("1.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: AssemblyVersion("1.3.7")]
+[assembly: AssemblyFileVersion("1.3.7")]
+[assembly: AssemblyInformationalVersion("1.3.7")]
